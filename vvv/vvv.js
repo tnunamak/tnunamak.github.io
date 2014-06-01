5ef3a678c3a30a41e5bde97182940bd4e5a82a10
Tim Nunamaker <tim@timnunamaker.com>
2014-05-31 22:33:33 -0500

    Use WASD for movement and make the graphics easier to understand

--- vvv/vvv.js
+++ vvv/vvv.js
@@ -1,53 +1,86 @@
 var width = 800,
     height = 600;
-var game = new Phaser.Game(width, height, Phaser.CANVAS, 'vvv', { preload: preload, create: create, update: update });
-var cursors;
 
+var game = new Phaser.Game(width, height, Phaser.CANVAS, 'lvls', { preload: preload, create: create, update: update });
+var keys;
 var player;
+var circle;
 
 function preload() {
-  game.load.spritesheet('ship', 'assets/ship.png', 32, 32, 8);
+    game.load.image('background','assets/starfield.png');
+    game.load.spritesheet('ship', 'assets/ship.png', 32, 32, 8);
 }
 
 function create() {
-  cursors = game.input.keyboard.createCursorKeys();
+    game.add.tileSprite(0, 0, 2000, 2000, 'background');
+    game.world.setBounds(0, 0, 1400, 1400);
 
-  player = game.add.sprite(width / 2, height / 2, 'ship');
-  game.physics.enable(player, Phaser.Physics.ARCADE);
+    player = game.add.sprite(width / 2, height / 2, 'ship');
+
+    game.physics.startSystem(Phaser.Physics.P2JS);
+    game.physics.enable(player, Phaser.Physics.P2JS);
+
+    game.camera.follow(player);
+
+    keys = {
+        w: game.input.keyboard.addKey(Phaser.Keyboard.W),
+        s: game.input.keyboard.addKey(Phaser.Keyboard.S),
+        a: game.input.keyboard.addKey(Phaser.Keyboard.A),
+        d: game.input.keyboard.addKey(Phaser.Keyboard.D)
+    };
 }
 
 function update() {
-  var distance = 6;
-  var diagonal = .707;
+    player.body.setZeroVelocity();
+    move();
+}
 
-  function updatePlayer(frame, x, y) {
-    player.frame = frame;
-    player.body.x += x * distance;
-    player.body.y -= y * distance;
-  }
-  if (cursors.up.isDown) {
+function render() {
 
-    if(cursors.right.isDown) {
-      updatePlayer(1, diagonal, diagonal);
-    } else if(cursors.left.isDown) {
-      updatePlayer(7, -diagonal, diagonal);
-    } else {
-      updatePlayer(0, 0, 1);
+}
+
+function move() {
+    var vec;
+    var distance = 200;
+    var diagonal_x = Math.cos(Math.PI / 6);
+    var diagonal_y = Math.cos(Math.PI / 3);
+
+    function updatePlayer(frame, x, y) {
+        player.frame = frame;
+        player.body.moveRight(x * distance);
+        player.body.moveUp(y * distance);
+    }
+    var vert, hor;
+
+    if(keys.w.isDown) {
+        vert = 'n';
+    }  else if(keys.s.isDown) {
+        vert = 's';
     }
 
-  } else if (cursors.down.isDown) {
-
-    if(cursors.right.isDown) {
-      updatePlayer(3, diagonal, -diagonal);
-    } else if(cursors.left.isDown) {
-      updatePlayer(5, -diagonal, -diagonal);
-    } else {
-      updatePlayer(4, 0, -1);
+    if(keys.d.isDown) {
+        hor = 'e';
+    } else if(keys.a.isDown) {
+        hor = 'w';
     }
 
-  } else if (cursors.left.isDown) {
-    updatePlayer(6, -1, 0);
-  } else if (cursors.right.isDown) {
-    updatePlayer(2, 1, 0);
-  }
+    if(!vert && !hor) {
+        return;
+    }
+    var cardinality = vert ? vert + (hor ? hor : '') : hor;
+
+    switch(cardinality) {
+        case 'n':  vec = [0, 0, diagonal_y]; break;
+        case 'ne': vec = [1, diagonal_x, diagonal_y]; break;
+        case 'e':  vec = [2, diagonal_x, 0]; break;
+        case 'se': vec = [3, diagonal_x, -diagonal_y]; break;
+        case 's':  vec = [4, 0, -diagonal_y]; break;
+        case 'sw': vec = [5, -diagonal_x, -diagonal_y]; break;
+        case 'w':  vec = [6, -diagonal_x, 0]; break;
+        case 'nw': vec = [7, -diagonal_x, diagonal_y]; break;
+    }
+
+    if(vec) {
+        updatePlayer.apply(this, vec);
+    }
 }
